779dd18978355ef73485eb19928b0fa72ad734d7
jacobfiler <jacobwilliamfiler@gmail.com>
2020-07-10 15:36:24 -0500

    Deleted everything and started over

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {TaskRow} from "./TaskRow"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default class App extends Component{
+  constructor()
+  {
+    super();
+
+    this.state ={
+      devStepList: [
+        {task: "Create the app", done: false},
+        {task: "Upload to GitHub", done: false},
+        {task: "Delete everything in it", done: false}
+      ]
+    } 
+  }
+
+  taskTableRow = (isTaskDone) => this.state.devStepList.filter(x => x.done === isTaskDone).map(notCompleted => <TaskRow 
+    key = {notCompleted.action}
+    item = {notCompleted}
+    callback = {this.toggleList}
+  />)
+
+  toggleList = (tasks) => this.setState({
+    devStepList: this.state.devStepList.map(item => item.task === tasks.task ? {...item, done: !item.done} : item)
+  })
+
+  render = ()=>
+  <div>
+ <div className="col-md-8 offset-md-2">
+        <table className="table table-striped table-bordered drop-shadow">
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Done</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.taskTableRow(false)}
+          </tbody>
+        </table>
+  </div>
+</div>
+};
